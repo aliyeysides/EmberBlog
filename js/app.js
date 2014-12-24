c7471c4333e217b2d439b10d1f22a698031fc152
aliyeysides <ali.yeysides@gmail.com>
2014-12-24 13:40:56 -0500

    remove layout example, nest blog route under index, create projects and project route, nest project under index

--- js/app.js
+++ js/app.js
@@ -3,9 +3,12 @@ App = Ember.Application.create({
 });
 
 App.Router.map(function() {
-	this.resource('blogs', function(){
+	this.route('index', {path: '/'}, function(){
 		this.resource('blog', {path: '/:title'});
-	});
+		this.resource('project', {path: '/:name'});
+	})
+	this.resource('blogs');
+	this.resource('projects');
 });
 
 // App.AboutController = Ember.Controller.extend({
@@ -24,6 +27,18 @@ App.BlogRoute = Ember.Route.extend({
 	}
 });
 
+App.ProjectsRoute = Ember.Route.extend({
+	model: function(){
+		return App.PROJECTS;
+	}
+})
+
+App.ProjectRoute = Ember.Route.extend({
+	model: function(params){
+		return App.PROJECTS.findby('name', params.name)
+	}
+})
+
 App.BLOGS = [{
 	title: 'This is a test article',
 	body: 'One fine day stuff happened laddy daahh',
@@ -32,3 +47,13 @@ App.BLOGS = [{
 	title: 'this is the second test article',
 	body: 'the enddddddddddd .......or is it?'
 }];
+
+App.PROJECTS = [{
+	name: 'pinpoint',
+	description: "it's a chrome extension",
+	builtWith: 'javascript'
+},{
+	name: 'livehub',
+	description: "it's a site n stuff",
+	builtWith: "rails"
+}]
